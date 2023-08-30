d8c8db27962bccdf9d7063965d1fb3b06d605e50
Matheus André Bezerra Mota <113203771+matheus1696@users.noreply.github.com>
2023-08-30 15:00:36 -0300

    V Mostra Científica V1.0.3 - Emissão Cerfificados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
-document.getElementById("form_certificado").addEventListener("submit", function(event) {
+document.getElementById("form-cert").addEventListener("submit", function(event) {
     event.preventDefault(); // Impede o envio normal do formulário
     
-    const cadastrado = document.getElementById("cadastrado").value; // Obtém o valor do campo de entrada
+    const cadastrado = document.getElementById("codigo-inscricao").value.toUpperCase(); // Obtém o valor do campo de entrada
     
     let cadastroEncontrado = false;
 
     for (let i = 0; i < lista_cadastrados.length; i++) {
-        if (lista_cadastrados[i].nome === cadastrado) {
-            nome = document.querySelector('.nome')
-            nome.innerHTML = cadastrado
-            nomeCandidado = document.querySelector('.nomeCandidado')
-            nomeCandidado.innerHTML = cadastrado
+        if (lista_cadastrados[i].inscricao === cadastrado) {
+
+            document.querySelector('.inscricao').innerHTML = lista_cadastrados[i].inscricao
+            document.querySelector('.trabalho').innerHTML = lista_cadastrados[i].trabalho
+            document.querySelector('.modalidade').innerHTML = lista_cadastrados[i].modalidade
+            document.querySelector('.relator').innerHTML = lista_cadastrados[i].relator
+
+            document.querySelector('.detalhes-trabalho').style.display="block"
+            document.querySelector('.detalhes-modalidade').style.display="block"
+            document.querySelector('.detalhes-relator').style.display="block"
+
+
+            document.querySelector('.titulo').innerHTML = lista_cadastrados[i].trabalho
+            
             cadastroEncontrado = true;
             if (cadastroEncontrado) {
                 window.print()
@@ -19,8 +28,13 @@ document.getElementById("form_certificado").addEventListener("submit", function(
         }
     }
 
-    if (!cadastroEncontrado) {
-        nome = document.querySelector('.nome')
-        nome.innerHTML = "Candidato não localizado."
+    if (!cadastroEncontrado) {        
+        document.querySelector('.detalhes-trabalho').style.display="none"
+        document.querySelector('.detalhes-modalidade').style.display="none"
+        document.querySelector('.detalhes-relator').style.display="none"
+        document.querySelector('.trabalho').innerHTML = ""
+        document.querySelector('.modalidade').innerHTML = ""
+        document.querySelector('.relator').innerHTML = ""
+        document.querySelector('.inscricao').innerHTML = "Inscrição não localizada."        
     }
 });
